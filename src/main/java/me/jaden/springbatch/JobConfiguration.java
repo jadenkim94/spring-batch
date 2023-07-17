6733a6a3df03bd4743a05f902f97d58bcb3f9f79
jaden94 <jonghaha7@gmail.com>
2023-07-17 14:50:52 +0900

    JobExecutionDecider 활용

--- src/main/java/me/jaden/springbatch/JobConfiguration.java
+++ src/main/java/me/jaden/springbatch/JobConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,42 +24,44 @@ public class JobConfiguration {
     @Bean
     public Job batchJob() {
         return jobBuilderFactory.get("job")
+                .incrementer(new RunIdIncrementer())
                 .start(step1())
-                    .on(ExitStatus.FAILED.getExitCode())
-                    .to(step2())
-                        .on("PASS")
-                        .stop()
+                .next(decider())
+                .from(decider()).on("ODD").to(oddStep())
+                .from(decider()).on("EVEN").to(eventStep())
                 .end()
                 .build();
+    }
+    @Bean
+    public JobExecutionDecider decider() {
+        return new CustomDecider();
     }
 
     @Bean
     public Step step1() {
-        return stepBuilderFactory.get("step1")
+        return stepBuilderFactory.get("startStep")
                 .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step 1 has executed");
-                    contribution.getStepExecution().setExitStatus(ExitStatus.FAILED);
+                    System.out.println(contribution.getStepExecution().getStepName() + " has executed");
                     return RepeatStatus.FINISHED;
                 }).build();
     }
 
     @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
+    public Step oddStep() {
+        return stepBuilderFactory.get("oddStep")
                 .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step 2 has executed");
+                    System.out.println(contribution.getStepExecution().getStepName() + " has executed");
                     return RepeatStatus.FINISHED;
                 })
-                .listener(new PassCheckingLister())
                 .build();
     }
 
-//    @Bean
-//    public Step step3() {
-//        return stepBuilderFactory.get("step3")
-//                .tasklet((contribution, chunkContext) -> {
-//                    System.out.println(contribution.getStepExecution().getStepName() + " has executed");
-//                    return RepeatStatus.FINISHED;
-//                }).build();
-//    }
+    @Bean
+    public Step eventStep() {
+        return stepBuilderFactory.get("eventStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(contribution.getStepExecution().getStepName() + " has executed");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
 }
