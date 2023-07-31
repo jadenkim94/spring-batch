11ddc61793ae1ec49dcdfb3fb2b6fdb05d29ba96
jaden94 <jonghaha7@gmail.com>
2023-07-31 15:18:21 +0900

    repeatTemplate 를 통한 반복제어

--- src/main/java/me/jaden/springbatch/JobConfiguration.java
+++ src/main/java/me/jaden/springbatch/JobConfiguration.java
@@ -5,7 +5,17 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.batch.repeat.RepeatCallback;
+import org.springframework.batch.repeat.RepeatContext;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.repeat.exception.SimpleLimitExceptionHandler;
+import org.springframework.batch.repeat.support.RepeatTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,29 +30,50 @@ public class JobConfiguration {
     @Bean
     public Job job() {
         return jobBuilderFactory.get("job")
+                .incrementer(new RunIdIncrementer())
                 .start(step1())
-                .next(step2())
                 .build();
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step 1 executed");
-                    return RepeatStatus.FINISHED;
+                .<String, String>chunk(5)
+                .reader(new ItemReader<>() {
+                    int i = 0;
+                    @Override
+                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+                        i++;
+                        return i > 5 ? null : "item" + i;
+                    }
                 })
+                .processor(new ItemProcessor<String, String>() {
+                    RepeatTemplate repeatTemplate = new RepeatTemplate();
+                    @Override
+                    public String process(String item) throws Exception {
+//                        CompositeCompletionPolicy compositeCompletionPolicy = new CompositeCompletionPolicy();
+//                        CompletionPolicy[] completionPolicies = {
+//                                new TimeoutTerminationPolicy(3000)};
+//                        compositeCompletionPolicy.setPolicies(completionPolicies);
+//                        repeatTemplate.setCompletionPolicy(compositeCompletionPolicy);
+                        repeatTemplate.setExceptionHandler(simpleLimitExceptionHandler());
+                        repeatTemplate.iterate(new RepeatCallback() {
+                            @Override
+                            public RepeatStatus doInIteration(RepeatContext context) throws Exception {
+                                System.out.println("repeatTemplateTest is testing");
+                                throw new RuntimeException("Exception is occurred");
+//                                return RepeatStatus.CONTINUABLE;
+                            }
+                        });
+                        return item;
+                    }
+                })
+                .writer(items -> System.out.println(items))
                 .build();
     }
 
     @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step 2 executed");
-//                    throw new RuntimeException("exception");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
+    public SimpleLimitExceptionHandler simpleLimitExceptionHandler() {
+        return new SimpleLimitExceptionHandler(3);
     }
 }
